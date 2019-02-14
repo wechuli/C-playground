af46d7ae8dd2a365b7f2a595ab38534acb2bd659
wechuli <wechulipaul@yahoo.com>
2019-02-14 15:43:17 +0300

    rearranging files

---
+++ basics/anothone.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //This is a preprocessor directive
+
+int main(void) //This identifies the function main()
+{               //This marks the beginning of main()
+    printf("Beware of the flying spaggetti monster");
+    return 0;  //This returns control to the operating system
+}
--- basics/helloworld.c
+++ basics/helloworld.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-
-int main(void)
-{
-    printf("Hello Wrold");
-    // return 0;
-	
+//The .c extension identifies the contents of the file as C source code
+int main(void){
+    printf("Hello world! How unoriginal, millions of words in English and you chose those two");
+    return 0;
+    
 }
+
+//The program is first compiled to object code files and then the object files are linked with a linker
